94fba39166ea2e0eb38dd6e2d356bea5cc903586
liudonghao <liu198456@126.com>
2020-02-04 17:56:56 +0800

    simplewebserver: use js deal nav item shift

---
+++ nodelearnings/simplewebserver/app/books_contents.js
@@ -0,0 +1,8 @@
+document.write('
+    <ul>\
+      <li>CSS_Mastery_advanced_web_standards_solutions</li>\
+      <li>CSS禅意花园(修订版)</li>\
+      <li>ECMA-262</li>\
+      <li>javascript权威指南中文第六版</li>\
+    </ul>\
+  ');
