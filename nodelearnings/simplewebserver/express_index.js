f2f2d28e6626d7044647e7b5e6b6c783d6570d28
liudonghao <liu198456@126.com>
2020-02-08 23:28:31 +0800

    simplewebserver:express_index add crud router

--- nodelearnings/simplewebserver/express_index.js
+++ nodelearnings/simplewebserver/express_index.js
@@ -12,6 +12,26 @@ server = http.createServer(app);
 app.get('/', function(request, respond){
   respond.send('Hello, Express');
 });
+app.get('/book/list', function(request, respond){
+  respond.contentType('json');
+  respond.send({title:'book list'});
+});
+app.post('/book/create', function(request, respond){
+  respond.contentType('json');
+  respond.send({title:'book created'});
+});
+app.get('/book/read/:id([0-9]+)', function(request, respond){
+  respond.contentType('json');
+  respond.send({title:'book with id ' + request.params.id + ' found'});
+});
+app.post('/book/update/:id([0-9]+)', function(request, respond){
+  respond.contentType('json');
+  respond.send({title:'book with id ' + request.params.id + ' updated'});
+});
+app.get('/book/delete/:id([0-9]+)', function(request, respond){
+  respond.contentType('json');
+  respond.send({title:'book with id ' + request.params.id + ' deleted'});
+});
 
 server.listen(HTTP_SERVER_PORT);
 console.log('Express server listening on port %d', HTTP_SERVER_PORT);
