aed000ebb0b4974d3aeb0d4095a4a386e3e09ed1
liudonghao <liu198456@126.com>
2020-02-08 23:08:12 +0800

    simplewebserver: add an express simple server

--- nodelearnings/simplewebserver/express_index.js
+++ nodelearnings/simplewebserver/express_index.js
@@ -2,3 +2,16 @@
 
 var http = require('http');
 var express = require('express');
+var app, server;
+const HTTP_SERVER_PORT = 3000
+
+app = express();
+server = http.createServer(app);
+
+//router
+app.get('/', function(request, respond){
+  respond.send('Hello, Express');
+});
+
+server.listen(HTTP_SERVER_PORT);
+console.log('Express server listening on port %d', HTTP_SERVER_PORT);
