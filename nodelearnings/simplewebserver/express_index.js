4ba4ac45d3072e86828dbb0efdd54ef4a535cc44
liudonghao <liu198456@126.com>
2020-02-09 10:33:43 +0800

    simplewebserver: use general crud

--- nodelearnings/simplewebserver/express_index.js
+++ nodelearnings/simplewebserver/express_index.js
@@ -12,24 +12,24 @@ server = http.createServer(app);
 app.get('/', function(request, response){
   response.send('Hello, Express');
 });
-app.all('/book/*?', function(request, response, next){
+app.all('/api/:objtype/*?', function(request, response, next){
   response.contentType('json');
   next();
 });
-app.get('/book/list', function(request, response){
-  response.send({title:'book list'});
+app.get('/api/:objtype/list', function(request, response){
+  response.send({title: request.params.objtype + ' list'});
 });
-app.post('/book/create', function(request, response){
-  response.send({title:'book created'});
+app.post('/api/:objtype/create', function(request, response){
+  response.send({title:request.params.objtype + ' created'});
 });
-app.get('/book/read/:id([0-9]+)', function(request, response){
-  response.send({title:'book with id ' + request.params.id + ' found'});
+app.get('/api/:objtype/read/:id([0-9]+)', function(request, response){
+  response.send({title:request.params.objtype + ' with id ' + request.params.id + ' found'});
 });
-app.post('/book/update/:id([0-9]+)', function(request, response){
-  response.send({title:'book with id ' + request.params.id + ' updated'});
+app.post('/api/:objtype/update/:id([0-9]+)', function(request, response){
+  response.send({title:request.params.objtype + ' with id ' + request.params.id + ' updated'});
 });
-app.get('/book/delete/:id([0-9]+)', function(request, response){
-  response.send({title:'book with id ' + request.params.id + ' deleted'});
+app.get('/api/:objtype/delete/:id([0-9]+)', function(request, response){
+  response.send({title:request.params.objtype + ' with id ' + request.params.id + ' deleted'});
 });
 
 server.listen(HTTP_SERVER_PORT);
