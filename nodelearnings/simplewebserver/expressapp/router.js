a27a8ee993810e73595965b5c35ee3370b6bbd5e
liudonghao <liu198456@126.com>
2020-02-11 23:31:12 +0800

    simplewebsever: support mongodb find books( curl http://127.0.0.1:3000/api/books/list

--- nodelearnings/simplewebserver/expressapp/router.js
+++ nodelearnings/simplewebserver/expressapp/router.js
@@ -1,8 +1,11 @@
 'use strict';
+const url = 'mongodb://localhost:27017';
+const dbName = 'expressapp';
 var configRouter;
 
 configRouter = function(app, server){
   app.get('/', function(request, response){
+    //connectAppdb();
     response.send('Hello, Express');
   });
   app.all('/api/:objtype/*?', function(request, response, next){
@@ -10,7 +13,31 @@ configRouter = function(app, server){
     next();
   });
   app.get('/api/:objtype/list', function(request, response){
-    response.send({title: request.params.objtype + ' list'});
+    const MongoClient = require('mongodb').MongoClient;
+    var mongoClient = new MongoClient(url);
+    mongoClient.connect(function(err, client) {
+      const db = client.db(dbName);
+	    if(err){
+	    	console.error(err);
+	    }else{
+	    	console.log("Connected correctly to server");
+        const collection = db.collection(request.params.objtype);
+        //const collection = db.collection('books');
+        collection.find({}).toArray(function(err, docs){
+          if(err){
+            console.error(err);
+          }
+          else{
+            console.log('Found ' + request.params.objtype + ' list');
+            console.log(docs);
+            response.send(docs);
+          }
+          console.log('Disconnected database server');
+          client.close();
+        });
+	    }
+    });
+    //response.send({title: request.params.objtype + ' list'});
   });
   app.post('/api/:objtype/create', function(request, response){
     response.send({title:request.params.objtype + ' created'});
