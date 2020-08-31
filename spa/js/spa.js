a597cdbb7b6ca0ca644196d839f9fb30c7d3de5b
liudh01 <liudh01@cashwaytech.com>
2020-08-31 16:26:59 +0800

    spa: add spa.shell.js and spa.shell.css

--- spa/js/spa.js
+++ spa/js/spa.js
@@ -8,11 +8,12 @@
 
 var spa = (function () {
     var initModule = function ( $container ) {
-        $container.html(
-            '<h1 style="display:inline-block; margin:25px;">'
-            +'hello world!'
-            +'</h1>'
-        );
+        //$container.html(
+        //    '<h1 style="display:inline-block; margin:25px;">'
+        //    +'hello world!'
+        //    +'</h1>'
+        //);
+        spa.shell.initModule ($container);
     };
 
     return { initModule : initModule };
--- spa/js/spa.shell.js
+++ spa/js/spa.shell.js
@@ -1,1 +1,52 @@
-/*js/spa.shell.js*/
+/*js/spa.shell.js
+ *shell module for spa
+ */
+
+/* global $, spa */
+spa.shell = (function () {
+    //---------------- BEGIN MODULE SCOPE VARIABLES
+    var configMap = {
+        main_html : String()
+        + '    <div class="spa-shell-head">'
+        + '        <div class="spa-shell-head-logo">logo</div>'
+        + '        <div class="spa-shell-head-acct">acct</div>'
+        + '        <div class="spa-shell-head-search">search</div>'
+        + '    </div>'
+        + '    <div class="spa-shell-main">'
+        + '        <div class="spa-shell-main-nav">nav</div>'
+        + '        <div class="spa-shell-main-content">content</div>'
+        + '    </div>'
+        + '    <div class="spa-shell-foot">'
+        + '    </div>'
+        + '    <div class="spa-shell-chat">'
+        + '        <div class="spa-shell-modal"></div>'
+        + '    </div>'
+    },
+        stateMap = { $container : null },
+        jqueryMap = {},
+        setJqueryMap, initModule;
+    //---------------- END MODULE SCOPE VARIABLES
+    
+    //---------------- BEGIN UTILITY SCOPE VARIABLES
+    //---------------- END UTILITY SCOPE VARIABLES
+    
+    //---------------- BEGIN DOM SCOPE VARIABLES
+    setJqueryMap = function (){
+        var $container = stateMap.$container;
+        jqueryMap = { $container : $container }
+    };
+    //---------------- END DOM SCOPE VARIABLES
+    
+    //---------------- BEGIN EVENT SCOPE VARIABLES
+    //---------------- END EVENT SCOPE VARIABLES
+    
+    //---------------- BEGIN PUBLIC SCOPE VARIABLES
+    initModule = function ( $container ) {
+        stateMap.$container = $container;
+        $container.html( configMap.main_html );
+        setJqueryMap();
+    };
+
+    return { initModule : initModule };
+    //---------------- END PUBLIC SCOPE VARIABLES
+}());
