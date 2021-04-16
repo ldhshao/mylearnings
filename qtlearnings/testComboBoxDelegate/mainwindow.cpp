1b4c1cdcb3e5fca111ca04a9d789d203cdceae8f
liudh01 <liudh01@cashwaytech.com>
2021-04-16 17:28:49 +0800

    weekly commit

--- qtlearnings/testComboBoxDelegate/mainwindow.cpp
+++ qtlearnings/testComboBoxDelegate/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "comboboxdelegate.h"
+//#include "cmbdelegate.h"
 
 #include <QStandardItemModel>
 #include <QVector>
@@ -8,8 +9,53 @@
 #include <QVariant>
 #include <QComboBox>
 #include <QMouseEvent>
+#include <QKeyEvent>
+#include <QTextCodec>
+#include <windows.h>
+#include <QDebug>
 
 //#include "cbdelegate.h"
+CKeyPressFilter::CKeyPressFilter(QObject *parent)
+    :QObject (parent)
+{
+
+}
+bool CKeyPressFilter::eventFilter(QObject *obj, QEvent *event)
+{
+    QEvent::Type tpCurr = event->type();
+    if (tpCurr == QEvent::KeyPress || tpCurr == QEvent::KeyRelease) {
+        static QString strModSn;
+        static QString strTest;
+        static QEvent::Type tpPre = QEvent::None;
+        static char ch;
+        QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+        if (!keyEvent->text().isEmpty() && tpPre != tpCurr)
+        {
+            if (QEvent::None != tpPre)
+            {
+                if (QEvent::KeyRelease == tpCurr)
+                {
+                    ch = static_cast<char>(keyEvent->key());
+                    strTest += keyEvent->text();
+                }
+            }
+            tpPre = tpCurr;
+        }
+        strModSn += keyEvent->text();
+        qDebug()<<tpCurr<<" "<<keyEvent->text();
+
+        if (Qt::Key_Return == keyEvent->key())
+        {
+            qDebug()<<strModSn;
+            qDebug()<<strTest;
+            strModSn.clear();
+            strTest.clear();
+            tpPre = QEvent::None;
+        }
+    }
+
+    return QObject::eventFilter(obj, event);
+}
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -21,9 +67,16 @@ MainWindow::MainWindow(QWidget *parent) :
     _model = new QStandardItemModel(this);
     ui->tableView->setModel( _model );
 
+    CKeyPressFilter *pKeyPressFlt = new CKeyPressFilter(this);
+    QCoreApplication::instance()->installEventFilter(pKeyPressFlt);
+
     connect(ui->pushButtonInit, SIGNAL(clicked()), this, SLOT(initModel()));
     connect( ui->pushButtonFirst, SIGNAL(clicked()), this, SLOT(firstType()));
     connect(ui->pushButtonPrintModel, SIGNAL(clicked()), this, SLOT(printModel()));
+    QString string = "D:\\cashway\\workspace\\svnchanyehua\\mcrscoretest\\config\\CC前维护整机外观作业指导书.xlsx";
+    QTextCodec *codec = QTextCodec::codecForName("GB2312");
+    QByteArray encodedString = codec->fromUnicode(string);
+    ShellExecuteA(0, "open", "excel.exe", encodedString.data(), 0, SW_SHOW);
 }
 
 MainWindow::~MainWindow()
@@ -140,6 +193,7 @@ void MainWindow::initModel()
     }
 
     ComboBoxDelegate *cbd = new ComboBoxDelegate(this);
+    //CmbDelegate *cbd = new CmbDelegate(this);
     ui->tableView->setItemDelegateForColumn(2, cbd);
     ui->tableView->openPersistentEditor( _model->index(3,2) );
     ui->tableView->openPersistentEditor( _model->index(4,2) );
@@ -186,4 +240,15 @@ void MainWindow::printModel()
     QString displayTxt = selection->currentIndex().model()->data( selection->currentIndex(), Qt::DisplayRole ).toString();
     QString userTxt = selection->currentIndex().model()->data( selection->currentIndex(), Qt::UserRole + 1 ).toString();
     ui->plainTextEdit->appendPlainText( QString("displayRole:%1--UserRole1:%2").arg(displayTxt).arg(userTxt) );
+}
+
+void MainWindow::keyPressEvent(QKeyEvent* event)
+{
+    static QString strMsg;
+    if(event->key() == Qt::Key_Return){//enter
+        qDebug() << strMsg;
+        strMsg.clear();
+    }else{
+        strMsg += event->text();
+    }
 }
--- qtlearnings/testComboBoxDelegate/mainwindow.h
+++ qtlearnings/testComboBoxDelegate/mainwindow.h
@@ -11,6 +11,15 @@ QT_BEGIN_NAMESPACE
 class QStandardItemModel;
 QT_END_NAMESPACE
 
+class CKeyPressFilter : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit CKeyPressFilter(QObject *parent=Q_NULLPTR);
+protected:
+    bool eventFilter(QObject *obj, QEvent *event);
+};
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -18,6 +27,9 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+protected:
+    void keyPressEvent(QKeyEvent *eventy) override;
 
 private slots:
     void initModel();
